186e3fb4fb6fdfdac2c68dad2098ac3305d0f3ee
amiraltaf1 <139712893+amiraltaf1@users.noreply.github.com>
2023-07-21 17:01:11 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					'https://apk057.online/' + encodeURI(path as string)
+					`https://apk057.online/` + encodeURI(path as string)
 				}`,
 			},
 		};
